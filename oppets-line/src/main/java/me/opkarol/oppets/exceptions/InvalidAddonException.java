744607458046a357a15cf7049307011210f3bcbb
ThisKarolGajda <jestemdobrywniczym@gmail.com>
2022-04-13 20:53:02 +0200

    0.8.3.5

--- oppets-line/src/main/java/me/opkarol/oppets/exceptions/InvalidAddonException.java
+++ oppets-line/src/main/java/me/opkarol/oppets/exceptions/InvalidAddonException.java
@@ -1,7 +1,15 @@
 package me.opkarol.oppets.exceptions;
 
+/**
+ * The type Invalid addon exception.
+ */
 public class InvalidAddonException extends RuntimeException {
 
+    /**
+     * Instantiates a new Invalid addon exception.
+     *
+     * @param errorMessage the error message
+     */
     public InvalidAddonException(String errorMessage) {
         super(errorMessage);
     }
